f9bef118f6c076dbd3d6bfe453b6a6309d561592
Lihua Li <lihua.li@ams.com>
2017-09-12 16:48:55 -0600

    Signed-off-by: Lihua Li <lihua.li@ams.com>

---
+++ src/main/java/com/alena/demo/CfgDemoApplication.java
@@ -0,0 +1,12 @@
+package com.alena.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfgDemoApplication.class, args);
+	}
+}
